35d63faf0691d6f0e9dd53d126302e8af38e3639
msulaev <ewrewr>
2023-02-26 14:57:25 +0200

    Module 3 task 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
+let str = 'list-style-image';
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  arr.map((item, index) => {
+    if (index != 0) {
+      arr[arr.indexOf(item)] = item.charAt(0).toUpperCase() + item.slice(1); }  
+  });
+  return arr.join('');
 }
+
+let result = camelize(str);
+console.log(result);
