b357c2e74c01fc01df6f18ea6c0226418717c4ae
msulaev <ewrewr>
2023-02-27 14:07:21 +0200

    Module 3 task 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,7 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ").map(item => item = +item).filter(item => !Number.isNaN(item));
+  return {
+    max: Math.max(...arr),
+    min: Math.min(...arr)
+  };
 }
