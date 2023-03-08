3daaf33f68d92ba27327f948e1408c6690561a72
msulaev <ewrewr>
2023-03-08 13:27:43 +0200

    Module 5 tsak 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
+let currentPos = 0;
+
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel');
+  let inner = document.querySelector('.carousel__inner');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let dist = document.querySelector('.carousel__inner').clientWidth;
+  arrowLeft.style.display = 'none'; 
+
+  carousel.addEventListener('click', (event) => {
+    if (event.target.closest('.carousel__arrow_right')) {
+      move(inner, -dist);
+    }
+    if (event.target.closest('.carousel__arrow_left')) {
+      move(inner, dist);
+    }
+    if (currentPos < 0) {
+      arrowLeft.style.display = '';
+    } else {
+      arrowLeft.style.display = 'none';
+    }
+
+    if (currentPos <= -(3 * dist)) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+    }
+  });
 }
+
+function move(elem, range) {
+  currentPos += range;
+  elem.style.transform = `translateX(${currentPos}px)`;
+}
+
+
